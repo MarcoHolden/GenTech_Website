4d773b7372e821252442d87cd741db10ee8c96d0
Mark Holden <mtholden17@gmail.com>
2024-06-28 05:56:31 -0400

    I implemted watch so that any updates to the front or back end would automatically show rather than have to manually rebuild the entire thing.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,7 +6,7 @@ async function run() {
     const root = document.getElementById('root');
 
     const { count } = await fetch('./api/count').then((res) => res.json());
-
+    
     ReactDOM.render(<h1>Hello, World! Count was {count}</h1>, root);
 }
 
