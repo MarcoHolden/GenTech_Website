726f52bbcefdf3fae2c96b0e3ea3300260173d3c
Kiduzk <70627866+Kiduzk@users.noreply.github.com>
2024-07-25 19:47:34 -0400

    Add real pictures

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,9 @@ export const App = () => {
     return <div id="app-container">
         <HomepageNavbar />
         <div id="member-profiles">
-            <MemberPicture />            
-            <MemberPicture />            
-            <MemberPicture />            
+            <MemberPicture src="./member_pictures/1.jpg" />            
+            <MemberPicture src="./member_pictures/2.jpg" />            
+            <MemberPicture src="./member_pictures/3.jpg" />            
         </div>
     </div>
 }
