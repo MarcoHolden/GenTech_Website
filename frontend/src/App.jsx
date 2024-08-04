d9c27af479ad5f44d86dc490ddf0b198421dcaf1
Mark Holden <mtholden17@gmail.com>
2024-08-03 23:12:27 -0400

    This makes the animated text centered and the nav bar is in the correct position

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@
 
 import React from "react";
 import { HomepageNavbar } from "./components/HomepageNavbar/HomepageNavbar";
+import { HomepageBody } from "./components/HomepageBody/HomepageBody";
 import "./App.css"
 
 export const App = () => {
     return <div id="app-container">
         <HomepageNavbar />
+        <HomepageBody />
     </div>
 }
---
+++ frontend/src/components/HomepageBody/HomepageBody.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./HomepageBody.css"
+
+
+export const HomepageBody = () => {
+    return <div className="containerAnimated1">
+        <p className="textAnimated1">&lt; Welcome to GenTech &gt;</p>
+    </div>
+}
