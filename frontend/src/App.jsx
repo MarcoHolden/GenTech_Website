23e589cde124208781c1a6c39ad1c82fc1706311
Kiduzk <70627866+Kiduzk@users.noreply.github.com>
2024-08-10 15:57:47 -0500

    Implement the new navbar design - add links and bottom border

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,18 @@
 
 import React from "react";
 import "./App.css"
-<<<<<<< HEAD
 import CursorTrail from './components/CursorTrail';
-=======
 import { HomepageNavbar } from "./components/HomepageNavbar/HomepageNavbar";
 import { MemberPicture } from "./components/MemberPicture/MemberPicture";
->>>>>>> d286edcc90e1d5cc6492506ff21a7fdd4d105230
 
 export const App = () => {
     return <div id="app-container">
         <HomepageNavbar />
-<<<<<<< HEAD
         <CursorTrail />
-=======
         <div id="member-profiles">
             <MemberPicture src="./member_pictures/1.jpg" />            
             <MemberPicture src="./member_pictures/2.jpg" />            
             <MemberPicture src="./member_pictures/3.jpg" />            
         </div>
->>>>>>> d286edcc90e1d5cc6492506ff21a7fdd4d105230
     </div>
 }
--- frontend/src/components/HomepageNavbar/HomepageNavbar.jsx
+++ frontend/src/components/HomepageNavbar/HomepageNavbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./HomepageNavbar.css"
-import image from "../../assets/gentech-logo.gif"
+import image from "../../assets/gentech-logo.svg"
 
 export const HomepageNavbar = () => {
     return <div id="navbar">
         <img src={image} alt="GenTech logo" id="logo" />
+
+        <div className="navbar-links">
+            <a href="">About</a>
+            <a href="">Projects</a>
+            <a href="">Resources</a>
+            <a href="">Members</a>
+            <a href="">Blog</a>
+        </div>
+
         <button className="navbar-button">Join</button>
         <button className="navbar-button">Login</button>
     </div>  
