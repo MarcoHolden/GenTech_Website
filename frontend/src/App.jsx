303e55697bb9deab54a7c182e1d55f5e9be69550
Mark Holden <mtholden17@gmail.com>
2024-08-10 22:55:24 -0400

    Got the animation into the current framework

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 
-import { HomepageNavbar } from "./components/HomepageNavbar/HomepageNavbar";
 import { HomepageBody } from "./components/HomepageBody/HomepageBody";
 import "./App.css"
 import CursorTrail from './components/CursorTrail';
@@ -13,11 +12,12 @@ export const App = () => {
     return <div id="app-container">
         <HomepageNavbar />
         <CursorTrail />
+        <HomepageBody />
         <div id="member-profiles">
             <MemberPicture src="./member_pictures/1.jpg" />            
             <MemberPicture src="./member_pictures/2.jpg" />            
             <MemberPicture src="./member_pictures/3.jpg" />            
         </div>
-        <HomepageBody />
+
     </div>
 }
