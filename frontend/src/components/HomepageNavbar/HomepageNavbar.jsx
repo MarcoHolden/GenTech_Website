8c82c1570a9cccb9cd4a19a6c5653b9a808c58b5
Kiduzk <70627866+Kiduzk@users.noreply.github.com>
2024-08-14 16:53:48 -0500

    Implement the logic for the navbar in small screen. If screen is small, then change links to icons

--- frontend/src/components/HomepageNavbar/HomepageNavbar.jsx
+++ frontend/src/components/HomepageNavbar/HomepageNavbar.jsx
@@ -1,20 +1,53 @@
-import React from "react";
-import "./HomepageNavbar.css"
-import image from "../../assets/gentech-logo.svg"
+import React, { useEffect, useState } from "react";
+import "./HomepageNavbar.css";
+import logoImage from "../../assets/gentech-logo.svg";
+import hamburgerIcon from "../../assets/hamburger-svg.svg";
+import cancelIcon from "../../assets/cancel-svg.svg";
+import { useMediaQuery } from "../../utils/useMediaQuery";
 
 export const HomepageNavbar = () => {
-    return <div id="navbar">
-        <img src={image} alt="GenTech logo" id="logo" />
+    // State that keeps track if the icon for small screens is open or closed
+    const [isOpen, setisOpen] = useState(false);
+    const isSmallScreen = useMediaQuery("(max-width: 700px)");
 
-        <div className="navbar-links">
-            <a href="">About</a>
-            <a href="">Projects</a>
-            <a href="">Resources</a>
-            <a href="">Members</a>
-            <a href="">Blog</a>
+    // When we come from big creen to small, always close the burger icon at the start
+    useEffect(() => setisOpen(false), [isSmallScreen]);
+
+    return (
+        <div id="navbar">
+            <img src={logoImage} alt="GenTech logo" id="logo" />
+
+            <div className="navbar-widescreen">
+                <div className="navbar-links">
+                    <a href="">About</a>
+                    <a href="">Projects</a>
+                    <a href="">Resources</a>
+                    <a href="">Members</a>
+                    <a href="">Events</a>
+                </div>
+
+                <button className="navbar-button">Join</button>
+                <button className="navbar-button">Login</button>
+            </div>
+
+            {/* Icon to make the screen responsive */}
+            <img
+                className="navbar-responsive-icon"
+                src={isOpen ? cancelIcon : hamburgerIcon}
+                alt="Hamburger button for responsiveness"
+                onClick={() => setisOpen(!isOpen)}
+            />
+
+            {/* If the burger icon is open, render the navbar links */}
+
+            {isOpen && isSmallScreen && <div className="navbar-links-burger">
+                <a href="">About</a>
+                <a href="">Projects</a>
+                <a href="">Resources</a>
+                <a href="">Members</a>
+                <a href="">Events</a>
+            </div>}
+        
         </div>
-
-        <button className="navbar-button">Join</button>
-        <button className="navbar-button">Login</button>
-    </div>  
-}
+    );
+};
---
+++ frontend/src/utils/useMediaQuery.jsx
@@ -0,0 +1,19 @@
+// This function tells us the width of our window. It takes in a media query and returns the matches for that query
+
+import { useState, useEffect } from "react";
+
+export const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener("resize", listener);
+    return () => window.removeEventListener("resize", listener);
+  }, [matches, query]);
+
+  return matches;
+}
