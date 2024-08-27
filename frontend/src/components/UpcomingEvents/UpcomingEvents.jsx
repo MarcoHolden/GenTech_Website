f00ea9b5d12c8bbbe9cc9b0deba39fe9ffbce482
Kiduzk <70627866+Kiduzk@users.noreply.github.com>
2024-08-26 21:27:55 -0500

    Center upcoming events and calendar

--- frontend/src/components/UpcomingEvents/UpcomingEvents.jsx
+++ frontend/src/components/UpcomingEvents/UpcomingEvents.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import "./UpcomingEvents.css"
+import "./UpcomingEvents.css";
 import { Calendar } from "./Calendar/Calendar";
-import { EventCard} from "./EventCard/EventCard";
+import { EventCard } from "./EventCard/EventCard";
 
 export const UpcomingEvents = () => {
-    return <div className="upcoming-events">
-        <div className="event-card-list">
-            <EventCard /> 
-            <EventCard /> 
-            <EventCard /> 
-        </div>
-        
-        <Calendar />
+  return (
+    <div className="upcoming-events">
+      <div className="event-card-list">
+        <div className="upcoming-events-title">Upcoming Events</div>
+        <EventCard />
+        <EventCard />
+        <EventCard />
+      </div>
+
+      <Calendar />
     </div>
-}
+  );
+};
